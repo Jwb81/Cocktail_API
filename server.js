9ddebae4c9b024bfe1b4973d54fbb049dbd91308
Jason Barnett <jayb21@neo.rr.com>
2018-09-04 01:46:50 -0400

    added response headers to allow the bartender webpages to connect to the api

--- server.js
+++ server.js
@@ -9,6 +9,13 @@ const port          = 1000;
 
 app.use(bodyParser.urlencoded({ extended : true }));
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+})
+
 MongoClient.connect(db.url, { useNewUrlParser: true }, (err, database) => {
     if (err) return console.log(err);
 
